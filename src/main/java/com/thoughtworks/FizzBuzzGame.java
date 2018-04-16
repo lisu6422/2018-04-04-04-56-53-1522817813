9da4a261474241c630ae869016b9a1fd926770bc
likly <569553290@qq.com>
2018-04-16 22:18:23 +0800

    done

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,15 +1,56 @@
 package com.thoughtworks;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    private Map<Integer, String> map = new HashMap<>();
+
+    {
+        map.put(3, "Fizz");
+        map.put(5, "Buzz");
+        map.put(7, "Whizz");
+    }
 
     public void start(int count) {
 
+        for (int i = 1; i <= count; i++) {
+            results.add(get(i));
+        }
+
+
     }
+
+    public String get(int i){
+        StringBuffer sb = new StringBuffer();
+        if (String.valueOf(i).contains("3")) {
+            sb.append(map.get(3));
+        } else if (i % 3 == 0) {
+            sb.append(map.get(3));
+
+            if (i % 5 == 0) {
+                sb.append(map.get(5));
+            }
+
+            if (i % 7 == 0) {
+                sb.append(map.get(7));
+            }
+
+        } else if (i % 5 == 0) {
+            sb.append(map.get(5));
+            if (i % 7 == 0) {
+                sb.append(map.get(7));
+            }
+        } else if (i % 7 == 0) {
+            sb.append(map.get(7));
+        } else {
+            sb.append(i);
+        }
+
+        return sb.toString();
+    }
+
 
     public List<String> getResults() {
         return results;
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,15 @@
 package com.thoughtworks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        FizzBuzzGame game = new FizzBuzzGame();
+        Assert.assertEquals("Fizz",game.get(3));
+        Assert.assertEquals("Fizz",game.get(13));
+        Assert.assertEquals("Fizz",game.get(30));
+        Assert.assertEquals("Fizz",game.get(43));
     }
 }
